3475a16e1c05a0e7090bb89e35111c2ee0569fcf
Maya S Rao <67920373+MayaSatishRao@users.noreply.github.com>
2021-07-11 10:53:53 +0530

    Add files via upload

---
+++ project-10/index.js
@@ -0,0 +1,31 @@
+const jokeElement = document.getElementById("joke");
+const btn = document.getElementById("jokeBtn");
+
+generateJoke();
+
+
+btn.addEventListener("click" , generateJoke);
+
+
+async function generateJoke(){
+
+    const config = {
+                 headers:{Accept:"application/json"}
+           };
+    const res = await fetch("https://icanhazdadjoke.com",config);
+    const data = await res.json();
+
+    jokeElement.innerHTML = data.joke;
+}
+
+// function generateJoke(){
+//     const config = {
+//         headers:{Accept:"application/json"}
+//     };
+
+//     fetch("https://icanhazdadjoke.com",config)
+//        .then(res=>res.json())
+//        .then(data=>{
+//          jokeElement.innerHTML = data.joke;
+//        })
+// }
